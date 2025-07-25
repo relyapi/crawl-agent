f629523e313f2ef88a1d6280122199d27e6e92b9
gaozhe <gaozhe@baiwang.com>
2025-07-25 15:42:22 +0800

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+import socketio
+import uvicorn
+from fastapi import FastAPI
+
+from app.router import router
+from app.task.server import sio
+
+app = FastAPI(root_path='/rely/server')
+socketio_app = socketio.ASGIApp(sio, app)
+
+
+@app.get("/healthz")
+def healthz() -> str:
+    return "ok"
+
+
+app.include_router(router)
+
+if __name__ == '__main__':
+    uvicorn.run(socketio_app, host='0.0.0.0', port=8500)
